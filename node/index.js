061c64ef239821d6baef8107e5eed17da1557dea
Yomguithereal <guillaumeplique@gmail.com>
2018-05-24 17:28:36 +0200

    .parse for node.js

--- node/index.js
+++ node/index.js
@@ -4,4 +4,13 @@
  *
  * Endpoint gathering both parser & writer for Node.js.
  */
+var xmldom = require('xmldom'),
+    createParserFunction = require('../common/parser.js');
+
+var doc = (new xmldom.DOMParser()).parseFromString('<t></t>', 'application/xml');
+
+exports.parse = createParserFunction(
+  xmldom.DOMParser,
+  doc.constructor
+);
 exports.write = require('../common/writer.js');
